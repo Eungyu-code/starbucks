8ed6a09b7243c380d5f7f5c400e41be3885eb315
LeeEunGyu <agyu625@gmail.com>
2022-03-06 16:09:17 +0900

    Initial commit

---
+++ src/test/java/toy/starbucks/StarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.starbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
